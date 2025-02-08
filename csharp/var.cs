7828bc01ef44c6ac490bcf4f3d0443c060303b8d
Xi-xi <160207677+Xixigolang@users.noreply.github.com>
2025-02-07 23:35:27 -0600

    add

---
+++ csharp/var.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "小明"; // 定义一个字符串变量
+        int age = 17; // 定义一个整数变量
+        double height = 1.75; // 定义一个小数变量
+        bool isStudent = true; // 定义一个布尔变量
+
+        Console.WriteLine("姓名: " + name);
+        Console.WriteLine("年龄: " + age);
+        Console.WriteLine("身高: " + height);
+        Console.WriteLine("是否是学生: " + isStudent);
+    }
+}
+
+//姓名: 小明
+//年龄: 17
+//身高: 1.75
+//是否是学生: True
